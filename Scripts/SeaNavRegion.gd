a5af99313fe7066e850b15f3391a47aec7f45aec
Jez <5053926+Ninjars@users.noreply.github.com>
2023-09-28 20:51:08 +0100

    attempting to debug nav polygon hole generation - looks to be a Godot bug hopefully fixed in 4.2

--- Scripts/SeaNavRegion.gd
+++ Scripts/SeaNavRegion.gd
@@ -5,11 +5,13 @@ class_name SeaNavRegion
 @export var boundsWidth: float
 @export var boundsHeight: float
 
-var halfWidth = boundsWidth / 2
-var halfHeight = boundsHeight / 2
-
 func buildNavRegion(islands: Array[Island]):
 	var navPoly = get_navigation_polygon()
+	navPoly.clear_outlines()
+	navPoly.clear_polygons()
+
+	var halfWidth = boundsWidth / 2
+	var halfHeight = boundsHeight / 2
 	navPoly.add_outline(
 		PackedVector2Array([
 			Vector2(-halfWidth, -halfHeight),
@@ -17,16 +19,43 @@ func buildNavRegion(islands: Array[Island]):
 			Vector2(halfWidth, halfHeight),
 			Vector2(-halfWidth, halfHeight),
 		]))
+#	navPoly.add_outline(
+#		PackedVector2Array([
+#			Vector2(-500, -500),
+#			Vector2(500, -500),
+#			Vector2(500, 500),
+#			Vector2(-500, 500),
+#		]))
+#
+#	navPoly.add_outline(
+#		PackedVector2Array([
+#			Vector2(-300, -300),
+#			Vector2(300, -300),
+#			Vector2(300, 300),
+#			Vector2(-300, 300),
+#		]))
+#
+#	navPoly.add_outline(
+#		PackedVector2Array([
+#			Vector2(-10, -10),
+#			Vector2(10, -10),
+#			Vector2(10, 10),
+#			Vector2(-10, 10),
+#		]))
+		
+	## TODO: re-add this in Godot 4.2 to see if holes work
 	#parseIslandCollider(navPoly, islands)
+	
 	navPoly.make_polygons_from_outlines()
+	
 	set_navigation_polygon(navPoly)
-	enabled = true
 
 func parseIslandCollider(navPolygon: NavigationPolygon, islands: Array[Island]):
+	print(str("parseIslandCollider for ", islands.size(), " islands"))
 	for island in islands:
-		var collisionpolygon: PackedVector2Array = island.collisionShape.polygon
-		var collisionpolygon_transform: Transform2D = island.get_global_transform()
+		var collisionPolygon: PackedVector2Array = island.collisionShape.polygon
+		var collisionPolygonTransform: Transform2D = island.collisionShape.get_global_transform()
 
-		var new_collision_outline: PackedVector2Array = collisionpolygon_transform * collisionpolygon
+		var new_collision_outline: PackedVector2Array = collisionPolygonTransform * collisionPolygon
 
 		navPolygon.add_outline(new_collision_outline)
